d7ed075796705b144751ec6c96ec9323fc70e8c4
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-03-25 16:17:02 +0200

    fix basename

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  // <React.StrictMode>
-  <Router>
-    <App />
-  </Router>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <Router basename="/countries-hub/">
+      <App />
+    </Router>
+    ,
+  </React.StrictMode>,
 );
