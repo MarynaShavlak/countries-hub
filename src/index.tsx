2012415bf02657d51b698076cb055d232a1ac4a4
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-03-25 16:40:03 +0200

    fix routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: (
+      <>
+        <App />
+      </>
+    ),
+  },
+]);
 root.render(
   <React.StrictMode>
-    <Router basename="/countries-hub/">
-      <App />
-    </Router>
-    ,
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
